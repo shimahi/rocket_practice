00267ae5cb6abff11e5478db1105dfc21bec6922
shimahi <shimahi@rear.jp>
2020-12-08 18:12:25 +0900

    rank

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,19 @@ fn hello(name: String, age: u8) -> String {
     format!("Hello, {} ({})", name, age)
 }
 
+// ランク
+#[get("/<age>")]
+fn hello_age(age: u8) -> String {
+    format!("Hello, ({})", age)
+}
+#[get("/<name>", rank = 2)]
+fn hello_name(name: String) -> String {
+    format!("Hello, {}", name)
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, hello])
+        .mount("/", routes![index, hello, hello_age, hello_name])
         .mount("/foo", routes![bar])
         .launch();
 }
