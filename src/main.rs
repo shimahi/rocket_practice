f8d3960a7b582c27e93cb2e4d85433bee413b58f
shimahi <shimahi@rear.jp>
2020-12-08 18:06:57 +0900

    routing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ fn index() -> &'static str {
     "Hello, World!"
 }
 
+// /foo/bar
+#[get("/bar")]
+fn bar() -> &'static str {
+    "FooBar"
+}
+
+// 動的なルーティング
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} ({})", name, age)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, hello])
+        .mount("/foo", routes![bar])
+        .launch();
 }
